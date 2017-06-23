1c4c47f2a274b5aae89120a523499eb029e0bed0
sharmaraghav260 <sharmaraghav260@gmail.com>
2017-06-23 13:35:27 -0600

    Login & Logout Done.
    Made the login form and authenticated the request giving us back a json web token and user data that we can then store in local storage. We also created the logout functionality.

--- angular-src/src/app/services/auth.service.ts
+++ angular-src/src/app/services/auth.service.ts
@@ -14,4 +14,24 @@ export class AuthService {
     headers.append('Content-Type', 'application/json');
     return this.http.post('http://localhost:6969/users/register', user, {headers: headers}).map(res => res.json());
   }
+
+  authenticateUser(user){
+    let headers = new Headers();
+    headers.append('Content-Type','application/json');
+    return this.http.post('http://localhost:6969/users/authenticate', user,{headers: headers})
+      .map(res => res.json());
+  }
+
+  storeUserData(token, user){
+    localStorage.setItem('id_token', token);
+    localStorage.setItem('user', JSON.stringify(user));
+    this.authToken = token;
+    this.user = user;
+  }
+
+  logout(){
+    this.authToken = null;
+    this.user = null;
+    localStorage.clear();
+  }
 }
