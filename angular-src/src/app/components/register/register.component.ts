bb4a7738e6367505850240652fd894c31401012f
sharmaraghav260 <sharmaraghav260@gmail.com>
2017-06-23 10:19:23 -0600

    Auth Service & User Registration Done.
    Created the authentication service and the user registration functionality using the backend API /users/register endpoint.

--- angular-src/src/app/components/register/register.component.ts
+++ angular-src/src/app/components/register/register.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ValidateService } from '../../services/validate.service';
+import { AuthService } from '../../services/auth.service';
 import { FlashMessagesService } from 'angular2-flash-messages';
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-register',
@@ -14,7 +16,12 @@ export class RegisterComponent implements OnInit {
   password: String;
 
 
-  constructor(private ValidateService: ValidateService, private flashMessage:FlashMessagesService) { }
+  constructor(
+    private ValidateService: ValidateService,
+    private flashMessage: FlashMessagesService,
+    private authService: AuthService,
+    private router: Router,
+  ) { }
 
   ngOnInit() {
   }
@@ -44,6 +51,17 @@ export class RegisterComponent implements OnInit {
       this.flashMessage.show('Please use at least one number, one lowercase, one uppercase letter and at least six characters.', {cssClass: 'alert-danger', timeout: 5000});
       return false;
     }
+
+    // Register user
+    this.authService.registerUser(user).subscribe(data => {
+      if(data.success){
+        this.flashMessage.show('You are now registered and can log in.', {cssClass: 'alert-success', timeout: 5000});
+        this.router.navigate(['/login']);
+      } else{
+        this.flashMessage.show('Something went wrong.', {cssClass: 'alert-danger', timeout: 5000});
+        this.router.navigate(['/register']);
+      }
+    });
   }
 
 }
